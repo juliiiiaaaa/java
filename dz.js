9c29856efb2d6b4a700d9bc3b2697665b12adf8b
juliiiiaaaa <80220662+juliiiiaaaa@users.noreply.github.com>
2021-03-15 22:56:42 +0300

    Add files via upload

---
+++ dz.js
@@ -0,0 +1,19 @@
+function outputNumbers() {
+let number = 100
+ for (let i=0; i <= number; i++) {
+  if (i === 5) {
+      console.log('пять')
+   } else if (i === 13) {
+      console.log('тринадцать')
+   } else if (i === 22) {
+       console.log('двадцать два')
+   } else if (i === 35) {
+       console.log ('тридцать пять')
+   } else if (i === 98) {
+       console.log('девяносто восемь')
+   } else {
+       console.log(i)
+   }
+}
+}
+outputNumbers()
